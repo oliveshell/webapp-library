822c36d6baa9d15f4305b1b169d8434863c4da86
oliveshell <xuhaocan@yeah.net>
2023-05-19 11:50:15 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import { AppInfo } from "@/types/app"
-export const APP_ID = 'custom-app-id'
-export const API_KEY = 'custom-app-key'
+export const APP_ID = '5f4eb818-4d91-4f5e-9134-a01fad95fbbd'
+export const API_KEY = 'app-fEfjqyc5Kc8TxDXMmFAADZy3'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   "title": 'Custom APP',
